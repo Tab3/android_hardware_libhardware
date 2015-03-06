60dfc23b66670c13508591471ab8dc43abb2f772
Michael Scott <michael.scott@linaro.org>
2015-03-06 11:29:47 -0800

    hardware: marvell: add private gralloc data structures for hw codecs
    This allows the framework to build w/o having vendor/marvell/* repos in place.
    And once proprietary codec libraries are added to the device, the framework can
    use them.
    
    Change-Id: Iad6c4575377fd05aa76117cfe453afba353493e4
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

---
+++ include/hardware/mrvl_gralloc_priv.h
@@ -0,0 +1,155 @@
+/****************************************************************************
+*
+*  Copyright 2014, The Android Open Source Project
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*****************************************************************************/
+
+#ifndef GRALLOC_PRIV_H_
+#define GRALLOC_PRIV_H_
+
+#include <stdint.h>
+#include <limits.h>
+#include <sys/cdefs.h>
+#include <hardware/gralloc.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <cutils/native_handle.h>
+#include <utils/Log.h>
+
+#include <linux/fb.h>
+
+#ifndef LOGE
+#   define LOGE(...) ALOGE(__VA_ARGS__)
+#endif
+
+struct private_module_t;
+struct private_handle_t;
+
+#define PRIVATE_HANDLE_FD_COUNT  2
+#define PRIVATE_HANDLE_INT_COUNT (int)((sizeof(private_handle_t) - sizeof(native_handle)) / sizeof(int) - PRIVATE_HANDLE_FD_COUNT)
+#define VIV_PRIV_SIZE 16
+
+struct private_module_t
+{
+    gralloc_module_t base;
+
+    struct private_handle_t* framebuffer;
+    uint32_t flags;
+    uint32_t numBuffers;
+    uint32_t bufferMask;
+    pthread_mutex_t lock;
+    buffer_handle_t currentBuffer;
+
+    struct fb_var_screeninfo info;
+    struct fb_fix_screeninfo finfo;
+    float xdpi;
+    float ydpi;
+    float fps;
+};
+
+/*****************************************************************************/
+
+#ifdef __cplusplus
+struct private_handle_t : public native_handle {
+
+    enum {
+        PRIV_FLAGS_FRAMEBUFFER      = 0x00000001,
+        PRIV_FLAGS_USES_PMEM        = 0x00000002,
+        PRIV_FLAGS_USES_PMEM_ADSP   = 0x00000004,
+        PRIV_FLAGS_NEEDS_FLUSH      = 0x00000008,
+        PRIV_FLAGS_NEEDS_INVALIDATE = 0x00000010
+    };
+
+#else
+struct private_handle_t {
+    native_handle_t nativeHandle;
+#endif
+    /* ion buffer fd */
+    int     master;
+
+    /* file-descriptors */
+    int     fd;
+
+    /* ints */
+    int     magic;
+    int     flags;
+    int     size;
+    uint64_t offset;
+
+    int     format;
+    int     width;
+    int     height;
+    int     mem_xstride;
+    int     mem_ystride;
+
+    /* Vivante private ints. */
+    uint64_t viv_priv[VIV_PRIV_SIZE];
+
+    uint64_t base;
+    int     pid;
+
+#ifdef __cplusplus
+    static const int sNumFds = PRIVATE_HANDLE_FD_COUNT;
+    static const int sMagic = 0x3141592;
+
+    private_handle_t(int fd, int size, int flags) :
+        fd(fd),
+        magic(sMagic),
+        flags(flags),
+        size(size),
+        offset(0),
+        format(0),
+        width(0),
+        height(0),
+        mem_xstride(0),
+        mem_ystride(0),
+        base(0),
+        pid(getpid())
+    {
+        version = sizeof(native_handle);
+        numInts = PRIVATE_HANDLE_INT_COUNT;
+        numFds = sNumFds;
+    }
+
+    ~private_handle_t()
+    {
+        magic = 0;
+    }
+
+    static int validate(const native_handle* h)
+    {
+        const private_handle_t* hnd = (const private_handle_t*)h;
+        if (!h || h->version != sizeof(native_handle) ||
+                h->numInts != PRIVATE_HANDLE_INT_COUNT || h->numFds != sNumFds ||
+                hnd->magic != sMagic)
+        {
+            LOGE("invalid gralloc handle (at %p)", h);
+            return -EINVAL;
+        }
+        return 0;
+    }
+
+    static private_handle_t* dynamicCast(const native_handle_t* in) {
+        if (validate(in) == 0) {
+            return (private_handle_t*) in;
+        }
+        return NULL;
+    }
+#endif
+};
+
+#endif /* GRALLOC_PRIV_H_ */
